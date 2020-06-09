b657c70090ebe580d897ca0408eb97312db02bfb
AvzTentacion <aviwemhlatyana249@gmail.com>
2020-06-09 15:45:32 +0200

    started the website

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Bootstrap CSS-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!--My OWN CSS-->
+    <link rel="stylesheet" href="style/style.css">
+    <title>Wiz Lyrics </title>
+  <style>
+      .wallpaper{ 
+    background-image: url(img/19xxx-superJumbo-v2.jpg);
+    background-size: cover;
+    background-position: right;
+    height: 500px;
+    background-color: rgba(0,0,0.5);
+}
+  </style>
+</head>
+<body>
+<?php 
+  require('hack/navbar.php');
+?>
+<div class="wallpaper">
+
+</div>
+<h1>Wiz Lyrics </h1>
+<div class="intro">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ad non fuga unde adipisci rerum obcaecati facere consectetur esse libero nihil sapiente quasi sint eum animi placeat, pariatur voluptas eos.
+</div>
+    
+<!--My own JS-->
+<script src="js/main.js"></script>
+<!--JQuery-->
+<script src="js/jquery.js"></script>
+<!--Bootstrap JS-->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
